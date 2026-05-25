1a3e2b5f117094d3332863f3147f8b6c21338f17
Devansh Krishna <devanshkrishna05@gmail.com>
2026-05-25 17:50:29 +0530

    collective migration cleanup

--- apps/pocketbase/pb_migrations/1778379714_updated_clickevents.js
+++ apps/pocketbase/pb_migrations/1778379714_updated_clickevents.js
@@ -1,20 +1,5 @@
-/// <reference path="../pb_data/types.d.ts" />
-migrate((app) => {
-  const collection = app.findCollectionByNameOrId("pbc_1556871182")
-
-  // update collection data
-  unmarshal({
-    "listRule": null
-  }, collection)
-
-  return app.save(collection)
-}, (app) => {
-  const collection = app.findCollectionByNameOrId("pbc_1556871182")
-
-  // update collection data
-  unmarshal({
-    "listRule": ""
-  }, collection)
-
-  return app.save(collection)
+migrate((db) => {
+  // disabled broken migration
+}, (db) => {
+  // rollback
 })
